8dec36d14ad02fbd8d459dd5d70550e83c91ab9c
Caíque Viana <126989214+caiquevianadev@users.noreply.github.com>
2023-10-16 17:14:09 -0300

    mse_script.py

--- mse_script.py
+++ mse_script.py
@@ -1,18 +1,18 @@
 import cv2
 import numpy as np
 
-# Carregue a imagem do mapa estático e do mapa dinâmico
+# Imagens carregadas
 mapa_estatico = cv2.imread("mapa_estatico.png")
 mapa_dinamico = cv2.imread("mapa_dinamico2.png")
 
-# Redimensione a imagem do mapa dinâmico para ter as mesmas dimensões do mapa estático
+# Redimensionamento das imagens
 mapa_dinamico = cv2.resize(mapa_dinamico, (mapa_estatico.shape[1], mapa_estatico.shape[0]))
 
-# Verifique se as imagens têm as mesmas dimensões
+# Verificação das dimensões das imagens
 assert mapa_estatico.shape == mapa_dinamico.shape, "Os mapas têm tamanhos diferentes!"
 
-# Calcule o Mean Squared Error (MSE)
+# Calculo do Mean Squared Error (MSE)
 mse = np.mean((mapa_estatico - mapa_dinamico) ** 2)
 
-# Exiba o valor do MSE
+# Valor do MSE
 print(f"Mean Squared Error (MSE): {mse}")
